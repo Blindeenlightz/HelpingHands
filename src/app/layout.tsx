1175d485082d1d4f50cc080016d0ef6ced7ac0bf
Jamie Braaksma <codingdad@outlook.com>
2025-05-29 15:26:23 -0600

    Add icon. Delete unused images.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,30 +4,38 @@ import "./globals.css";
 import { Providers } from "./providers";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Helping Hands",
-  description: "A website featuring a collection of charities that can be donated to.",
+    title: "Helping Hands",
+    description:
+        "A website featuring a collection of charities that can be donated to.",
+    icons: {
+        icon: "/world_hands_icon.ico",
+        shortcut: "/world_hands_icon.png",
+        apple: "/world_hands_icon.png",
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				<Providers>{children}</Providers>
-			</body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
 }
